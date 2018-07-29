2b1ac437b5d02b93bb82f2de62033a608b4f0a35
Kyle Conkright <kyleconkright@gmail.com>
2018-07-29 11:36:11 -0700

    organizing

--- app/client/src/containers/billing/billing.tsx
+++ app/client/src/containers/billing/billing.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Elements, StripeProvider } from 'react-stripe-elements';
-import CheckoutForm from './../checkout-form';
+import CheckoutForm from './checkout-form';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -25,24 +25,24 @@ class Billing extends React.Component<BillingProps, BillingState> {
                 <header>
                     <h1>Payment Info</h1>
                 </header>
-
                 <div>
-                    <p className="text-bold">Shipping Address</p>
-                    <p>{this.props.order.customerName}</p>
-                    <p>{this.props.address.address1}</p>
-                    {this.props.address.address2 ? <p>{this.props.address.address2}</p> : null }
-                    <p>{this.props.address.city}, {this.props.address.state} {this.props.address.zip}</p>
-                    <p></p>
-                </div>
-
-                <StripeProvider apiKey="pk_test_LwL4RUtinpP3PXzYirX2jNfR">
                     <div>
-                        <Elements>
-                            <CheckoutForm />
-                        </Elements>
+                        <p className="text-bold">Shipping Address</p>
+                        <p>{this.props.order.customerName}</p>
+                        <p>{this.props.address.address1}</p>
+                        {this.props.address.address2 ? <p>{this.props.address.address2}</p> : null }
+                        <p>{this.props.address.city}, {this.props.address.state} {this.props.address.zip}</p>
+                        <p></p>
                     </div>
-                </StripeProvider>
 
+                    <StripeProvider apiKey="pk_test_LwL4RUtinpP3PXzYirX2jNfR">
+                        <div>
+                            <Elements>
+                                <CheckoutForm />
+                            </Elements>
+                        </div>
+                    </StripeProvider>
+                </div>
             </section>
         )
     }
